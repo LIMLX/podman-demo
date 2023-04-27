1e515b1e40711e66f23d7f57b64078cd68081101
LIMLX <1838941465@qq.com>
2023-04-27 20:30:04 +0800

    修复未知bug?api错误

--- src/microservice/controller/leave/division.controller.ts
+++ src/microservice/controller/leave/division.controller.ts
@@ -6,23 +6,23 @@ import { LeaveDivisionService } from "src/microservice/service/leave";
 @ApiTags("leave")
 @Controller('leave/division')
 export class LeaveDivisionController {
-    constructor(private readonly divisionService: LeaveDivisionService) {}
+  constructor(private readonly divisionService: LeaveDivisionService) { }
 
-    @ApiOperation({summary:"获取省级信息的接口", description:"获取到一系列省份信息"})
-    @Get('/province')
-    async province () {
-      return await this.divisionService.province()
-    }
-  
-    @ApiOperation({summary:"获取市级信息的接口", description:"根据省份id查询对应的市级信息"})
-    @Get('/city/province=:provinceId')
-    async city (@Param() cityDto:CityDto) {
-      return this.divisionService.city(cityDto)
-    }
-  
-    @ApiOperation({summary:"获取县级信息的接口", description:"根据市级id查询对应的县级信息"})
-    @Get('/country/city=:cityId')
-    async country (@Param() countryDto:CountryDto) {
-      return this.divisionService.country(countryDto)
-    }
+  @ApiOperation({ summary: "获取省级信息的接口", description: "获取到一系列省份信息" })
+  @Get('province')
+  async province() {
+    return await this.divisionService.province()
+  }
+
+  @ApiOperation({ summary: "获取市级信息的接口", description: "根据省份id查询对应的市级信息" })
+  @Get('city/:provinceId')
+  async city(@Param() cityDto: CityDto) {
+    return this.divisionService.city(cityDto)
+  }
+
+  @ApiOperation({ summary: "获取县级信息的接口", description: "根据市级id查询对应的县级信息" })
+  @Get('country/:cityId')
+  async country(@Param() countryDto: CountryDto) {
+    return this.divisionService.country(countryDto)
+  }
 }
