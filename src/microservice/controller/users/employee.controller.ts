2ab18c59cdb1f44f9a71c9d9689372e193b47168
LIMLX <1838941465@qq.com>
2023-05-09 15:49:58 +0800

    效验更新

--- src/microservice/controller/users/employee.controller.ts
+++ src/microservice/controller/users/employee.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Patch, Post, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { ApiOperation, ApiTags } from "@nestjs/swagger";
-import { EmployeeRole } from "src/common";
+import { EmployeeRole, User } from "src/common";
 import { EmployeeRoleGuard, StudentRoleGuard } from "src/common/guards";
 import { CreateEmployeeDto, LoginEmloyeeDto, UpdateEmployeeDto, UpdateEmployeePswDto } from "src/microservice/dto";
 import { UserEmployeeService } from "src/microservice/service/users";
@@ -12,42 +12,48 @@ import { UserEmployeeService } from "src/microservice/service/users";
 export class UserEmployeeController {
   constructor(
     private readonly usersService: UserEmployeeService
-  ) {}
-    
-  @ApiOperation({summary:"职工登录接口", description:"同时进行数据库登录查询,并且返回jwt数据"})
+  ) { }
+
+  @ApiOperation({ summary: "职工登录接口", description: "同时进行数据库登录查询,并且返回jwt数据" })
   @Post("/loginEmployee")
-  async loginEmployee(@Body() loginEmloyeeDto : LoginEmloyeeDto ) {
+  async loginEmployee(@Body() loginEmloyeeDto: LoginEmloyeeDto) {
     return await this.usersService.loginEmployee(loginEmloyeeDto)
   }
-  
-  @ApiOperation({summary:"职工注册接口", description:"录入职工信息"})
+
+  @ApiOperation({ summary: "职工注册接口", description: "录入职工信息" })
   @Post("/signInEmployee")
-  async signInEmployee (@Body() createEmployeeDto: CreateEmployeeDto) {
+  async signInEmployee(@Body() createEmployeeDto: CreateEmployeeDto) {
     return await this.usersService.signInEmployee(createEmployeeDto)
   }
 
-  @ApiOperation({summary:"职工信息修改接口", description:"修改职工信息"})
+  @ApiOperation({ summary: "职工信息修改接口", description: "修改职工信息" })
   @Patch("/updateEmployee")
-  async updateEmployee (@Body() updateEmployeeDto: UpdateEmployeeDto) {
+  async updateEmployee(@Body() updateEmployeeDto: UpdateEmployeeDto) {
     return await this.usersService.updateEmployee(updateEmployeeDto)
   }
-  
-  @ApiOperation({summary:"职工密码修改接口", description:"修改职工密码"})
+
+  @ApiOperation({ summary: "职工密码修改接口", description: "修改职工密码" })
   @Patch("/updateEmployeePsw")
-  async updateEmployeePsw (@Body() updateEmployeePswDto: UpdateEmployeePswDto) {
-    return await this.usersService.updateEmployeePsw(updateEmployeePswDto) 
+  async updateEmployeePsw(@Body() updateEmployeePswDto: UpdateEmployeePswDto) {
+    return await this.usersService.updateEmployeePsw(updateEmployeePswDto)
   }
 
-  @ApiOperation({summary:"获取所有职工的基础信息", description:"职工基础信息获取"})
+  @ApiOperation({ summary: "获取所有职工的基础信息", description: "职工基础信息获取" })
   @Get("/findEmployeeAll")
-  async findEmployeeAll () {
+  async findEmployeeAll() {
     return await this.usersService.findEmployeeAll()
   }
-  
+
   @Get('demo')
   @UseGuards(AuthGuard('jwt'))
-  @EmployeeRole([{module:"History",level:1}])
-  async demo (){
+  @EmployeeRole([{ module: "History", level: 1 }])
+  async demo() {
     return "ok"
+  }
+
+  @Get('token')
+  @UseGuards(AuthGuard('jwt'))
+  async token(@User('type') user) {
+    return user
   }
 }
